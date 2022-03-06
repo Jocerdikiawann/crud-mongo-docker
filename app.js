551c76307362594dc897f1ff1e9bd0d858961d9e
Jocerdikiawann <cocokcocok33@gmail.com>
2022-03-06 21:37:58 +0700

    fix: remove comment

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//db
-// const db = require('./domain/repositories/repository-mongo')
-
 var crudRouter = require('./routes')
 
 var app = express();
@@ -17,7 +14,6 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// db.db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use('/api/v1', crudRouter)
 
